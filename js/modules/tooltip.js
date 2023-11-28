4226eb5a82b4502601a16ae4283a1e34bb82a838
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2023-11-27 23:53:49 -0300

    javascript para o funcionamento to tooltip

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,5 +1,40 @@
-export default function initTooltip() {
+export default function initTooltip() {}
 
+const tooltips = document.querySelector("[data-tooltip]");
+
+tooltips.addEventListener("mouseover", onMouseOver);
+
+function onMouseOver(event) {
+  const tooltipBox = criarTooltipBox(this);
+
+  onMouseMove.tooltipBox = tooltipBox;
+  this.addEventListener("mousemove", onMouseMove);
+
+  onMouseLeave.tooltipBox = tooltipBox;
+  onMouseLeave.element = this;
+  this.addEventListener("mouseleave", onMouseLeave);
 }
 
-const tooltip = document.querySelector('[data-tooltip]');
+const onMouseLeave = {
+  handleEvent() {
+    this.tooltipBox.remove();
+    this.element.removeEventListener("mouseleave", onMouseLeave);
+    this.element.removeEventListener("mousemove", onMouseMove);
+  },
+};
+
+const onMouseMove = {
+  handleEvent(event) {
+    this.tooltipBox.style.top = event.pageY + 10 + "px";
+    this.tooltipBox.style.left = event.pageX + 10 + "px";
+  },
+};
+
+function criarTooltipBox(element) {
+  const tooltipBox = document.createElement("div");
+  const text = element.getAttribute("aria-label");
+  tooltipBox.classList.add("tooltip");
+  tooltipBox.innerText = text;
+  document.body.appendChild(tooltipBox);
+  return tooltipBox;
+}
