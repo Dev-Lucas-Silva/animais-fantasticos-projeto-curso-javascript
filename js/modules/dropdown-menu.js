e6a6052424cca5154bc88641ab54d67b849676bc
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2023-11-29 00:37:44 -0300

    inicio de javascipt do dropdown-mwnu: adicao de eventos

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,15 @@
 export default function initDropdownMenu() {
 
 }
+const dopdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dopdownMenus.forEach((menu) => {
+    ['touchstart', 'click'].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.toggle('active');
+};
