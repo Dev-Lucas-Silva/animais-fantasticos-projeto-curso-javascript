06ae46869381b0dea331310b80c371f8e4d46717
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2024-01-10 22:34:35 -0300

    module outsideclick para o dropdown menu

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,15 +1,20 @@
-export default function initDropdownMenu() {
+import outsideClick from "./outsideclick.js";
 
-}
-const dopdownMenus = document.querySelectorAll('[data-dropdown]');
 
-dopdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
+export default function initDropdownMenu() {}
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
     menu.addEventListener(userEvent, handleClick);
   });
 });
 
 function handleClick(event) {
   event.preventDefault();
-  this.classList.toggle('active');
-};
+  this.classList.add("active");
+  outsideClick(this, ["touchstart", "click"], () => {
+    this.classList.remove("active");
+  });
+}
+
